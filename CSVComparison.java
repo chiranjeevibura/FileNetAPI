56b5f178fe50009b2ce917ce350535fbcfe6ffa6
chiranjeevibura <135640578+chiranjeevibura@users.noreply.github.com>
2023-09-22 07:46:37 -0500

    Update CSVComparison.java

--- CSVComparison.java
+++ CSVComparison.java
@@ -4,9 +4,9 @@ import java.util.concurrent.*;
 
 public class CSVComparison {
     public static void main(String[] args) {
-        String inputCsvFile1 = "input1.csv";
-        String inputCsvFile2 = "input2.csv";
-        String outputCsvFile = "output.csv";
+        String inputCsvFile1 = "C:\\Work\\input1.csv";
+        String inputCsvFile2 = "C:\\Work\\input2.csv";
+        String outputCsvFile = "C:\\Work\\output.csv";
         String columnToCompare = "loan_number"; // Change to the specific column name
 
         try {
@@ -17,7 +17,7 @@ public class CSVComparison {
             List<String> sortedChunks2 = sortLargeCSV(inputCsvFile2, columnToCompare);
 
             // Step 2: Merge the sorted chunks to find unique records
-            Set<String> uniqueRecords = mergeSortedChunks(sortedChunks1, sortedChunks2, columnToCompare);
+            List<String> uniqueRecords = mergeSortedChunks(sortedChunks1, sortedChunks2, columnToCompare);
 
             // Step 3: Write the unique records to the output CSV file
             writeUniqueRecordsToCSV(uniqueRecords, outputCsvFile);
@@ -43,23 +43,22 @@ public class CSVComparison {
             while ((line = br.readLine()) != null) {
                 chunk.add(line);
                 if (chunk.size() >= chunkSize) {
-                    Collections.sort(chunk, new CSVComparator(columnToCompare));
+                    Collections.sort(chunk, new CSVComparator(header, columnToCompare));
                     sortedChunks.addAll(chunk);
                     chunk.clear();
                 }
             }
 
             // Sort and add the remaining lines in the last chunk
-            Collections.sort(chunk, new CSVComparator(columnToCompare));
+            Collections.sort(chunk, new CSVComparator(header, columnToCompare));
             sortedChunks.addAll(chunk);
         }
 
         return sortedChunks;
     }
 
-    private static Set<String> mergeSortedChunks(List<String> sortedChunks1, List<String> sortedChunks2, String columnToCompare) {
-        Set<String> uniqueRecords = new ConcurrentSkipListSet<>(new CSVComparator(columnToCompare));
-
+    private static List<String> mergeSortedChunks(List<String> sortedChunks1, List<String> sortedChunks2, String columnToCompare) {
+        List<String> uniqueRecords = new ArrayList<>();
         Iterator<String> iterator1 = sortedChunks1.iterator();
         Iterator<String> iterator2 = sortedChunks2.iterator();
 
@@ -67,7 +66,7 @@ public class CSVComparison {
         String record2 = iterator2.hasNext() ? iterator2.next() : null;
 
         while (record1 != null && record2 != null) {
-            int compareResult = new CSVComparator(columnToCompare).compare(record1, record2);
+            int compareResult = new CSVComparator(sortedChunks1.get(0), columnToCompare).compare(record1, record2);
             if (compareResult < 0) {
                 uniqueRecords.add(record1);
                 record1 = iterator1.hasNext() ? iterator1.next() : null;
@@ -95,7 +94,7 @@ public class CSVComparison {
         return uniqueRecords;
     }
 
-    private static void writeUniqueRecordsToCSV(Set<String> uniqueRecords, String outputFile) throws IOException {
+    private static void writeUniqueRecordsToCSV(List<String> uniqueRecords, String outputFile) throws IOException {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
             for (String record : uniqueRecords) {
                 bw.write(record);
@@ -105,9 +104,11 @@ public class CSVComparison {
     }
 
     static class CSVComparator implements Comparator<String> {
+        private final String header;
         private final String columnToCompare;
 
-        CSVComparator(String columnToCompare) {
+        public CSVComparator(String header, String columnToCompare) {
+            this.header = header;
             this.columnToCompare = columnToCompare;
         }
 
@@ -115,18 +116,19 @@ public class CSVComparison {
         public int compare(String record1, String record2) {
             String[] fields1 = record1.split(",");
             String[] fields2 = record2.split(",");
-            int columnIndex = getColumnIndex(fields1, columnToCompare);
+
+            int columnIndex = getColumnIndex(header.split(","), columnToCompare);
 
             return fields1[columnIndex].compareTo(fields2[columnIndex]);
         }
 
-        private int getColumnIndex(String[] fields, String columnName) {
-            for (int i = 0; i < fields.length; i++) {
-                if (fields[i].equals(columnName)) {
+        private int getColumnIndex(String[] headers, String columnName) {
+            for (int i = 0; i < headers.length; i++) {
+                if (headers[i].equals(columnName)) {
                     return i;
                 }
             }
-            return -1; // Column not found
+            return -1; // Column not found in header
         }
     }
 }
